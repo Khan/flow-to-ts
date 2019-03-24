b4513670e6b16096aac67c63c116ba36243972f6
Kevin Barabash <kevinb@khanacademy.org>
2019-03-24 19:08:10 -0400

    initial commit of playground

---
+++ playground/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import App from "./app";
+
+const container = document.querySelector("#container");
+
+ReactDOM.render(<App />, container);
