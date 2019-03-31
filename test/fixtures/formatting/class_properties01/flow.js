1a1a409442a579095897bd638a57665776b5ef75
Kevin Barabash <kevinb@khanacademy.org>
2019-03-31 19:03:16 -0400

    forgot to include test case

---
+++ test/fixtures/formatting/class_properties01/flow.js
@@ -0,0 +1,15 @@
+class Foo {
+
+    // line comment
+    foo: string;
+
+    /**
+     * block comment
+     */
+    bar: number;
+
+    // comment on method
+    baz() {
+        console.log("baz");
+    }
+}
