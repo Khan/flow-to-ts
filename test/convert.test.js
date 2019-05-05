8f32768c5dceabc77fe12bf5258984f101f44f44
Kevin Barabash <kevinb@khanacademy.org>
2019-05-04 20:39:40 -0400

    create test harness for type-checking tests, move existing fixtures into a subdirectory

--- test/convert.test.js
+++ test/convert.test.js
@@ -6,14 +6,21 @@ const convert = require("../src/convert.js");
 const failingTestNames = ["spread03", "spread04"];
 
 describe("convert", () => {
-  const suites = fs.readdirSync(path.join(__dirname, "fixtures"));
+  const suites = fs.readdirSync(path.join(__dirname, "fixtures/convert"));
   for (const suiteName of suites) {
     describe(suiteName, () => {
-      const tests = fs.readdirSync(path.join(__dirname, "fixtures", suiteName));
+      const tests = fs.readdirSync(
+        path.join(__dirname, "fixtures/convert", suiteName)
+      );
       for (const testName of tests.filter(
         testName => !failingTestNames.includes(testName)
       )) {
-        const dir = path.join(__dirname, "fixtures", suiteName, testName);
+        const dir = path.join(
+          __dirname,
+          "fixtures/convert",
+          suiteName,
+          testName
+        );
         const flowCode = fs.readFileSync(path.join(dir, "flow.js"), "utf-8");
         const tsCode = fs.readFileSync(path.join(dir, "ts.js"), "utf-8");
         const hasOptions = fs.existsSync(path.join(dir, "options.json"));
---
+++ test/matchers.js
@@ -0,0 +1,47 @@
+const diff = require("jest-diff");
+
+expect.extend({
+  toMatchErrors(received, expected) {
+    received = received.map(diagnostic => {
+      return {
+        start: diagnostic.start,
+        length: diagnostic.length,
+        code: diagnostic.code,
+        messageText: diagnostic.messageText
+      };
+    });
+    const pass = JSON.stringify(received) === JSON.stringify(expected);
+
+    const options = {
+      isNot: this.isNot,
+      promise: this.promise
+    };
+
+    const message = pass
+      ? () =>
+          this.utils.matcherHint(
+            "toMatchErrors",
+            undefined,
+            undefined,
+            options
+          ) +
+          "\n\n" +
+          `Expected: ${this.utils.printExpected(expected)}\n` +
+          `Received: ${this.utils.printReceived(received)}`
+      : () => {
+          const difference = diff(expected, received, {
+            expand: this.expand
+          });
+          return (
+            this.utils.matcherHint("toEqual", undefined, undefined, options) +
+            "\n\n" +
+            (difference && difference.includes("- Expect")
+              ? `Difference:\n\n${difference}`
+              : `Expected: ${this.utils.printExpected(expected)}\n` +
+                `Received: ${this.utils.printReceived(received)}`)
+          );
+        };
+
+    return { actual: received, message, pass };
+  }
+});
