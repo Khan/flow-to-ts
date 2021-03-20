dacae6b26d2920c927a4d6d00abbc37567e64b91
Kevin Barabash <kevinb@khanacademy.org>
2021-03-20 16:27:55 -0400

    upgrade jest and fix coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   testMatch: ["<rootDir>/test/*.test.ts"],
   // TODO: write a custom transform that checks for @flow comments and then
   // uses babel to transform the file with either the flow preset or the typescript
@@ -7,7 +8,4 @@ module.exports = {
     "^.+\\.(ts|tsx|js)$": "<rootDir>/jest.transformer.js",
   },
   modulePathIgnorePatterns: ["babel/packages/babel-core"],
-  moduleNameMapper: {
-    "^@babel/(.*)$": "<rootDir>/babel/packages/babel-$1/src/index",
-  },
 };
